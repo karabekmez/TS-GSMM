350ef0ba78b0d57c5e06286ba21b4bfb0479cc42
karabekmez <50235417+karabekmez@users.noreply.github.com>
2023-11-29 12:58:58 +0300

    Update wasdis.m

--- code/wasdis.m
+++ code/wasdis.m
@@ -1,14 +1,14 @@
-tic
-for x=1:1881
-    for y=x:1881
-        for t=1:12
-            D(x,y,t)=ws_distance(SEF_N(x,1:2000,t),SEF_N(y,1:2000,t));
+% calculate 1-Wasserstein distance between time series
+load SEF_N.mat % 3D array (reactions, samples, time points)
+sz=size(SEF_N);
+for x=1:sz(1)
+    for y=x:sz(1)
+        for t=1:sz(3)
+            D(x,y,t)=ws_distance(SEF_N(x,:,t),SEF_N(y,:,t)); 
             D(y,x,t)=D(x,y,t);
         end
-        DD(x,y)=(sum((D(x,y,:).^2).^(1/12))) ;
+        DD(x,y)=(sum((D(x,y,:).^2).^(1/sz(3)))) ;
         DD(y,x)=DD(x,y);
     end
-    x
 end
 save DD DD
-toc
